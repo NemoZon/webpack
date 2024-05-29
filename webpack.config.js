29869b5b251dae21b001e8dacc64eed67db47a3e
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-05-29 15:07:35 +0200

    refactor: env vars added in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 // https://webpack.js.org/guides/getting-started/#basic-setup
-module.exports = {
-  mode: 'production', // development || production
+module.exports = (env) => ({
+  mode: env.mode ?? 'development', // development || production
   entry: './src/index.js',
   output: {
     filename: '[name].[contenthash].js', // https://webpack.js.org/configuration/output/#template-strings
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
-};
+});
